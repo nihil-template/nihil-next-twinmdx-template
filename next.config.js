f321fc8de7c9c1aa037297bd4d0a65bdc1e66d51
NIHILncunia <nihil_ncunia@naver.com>
2022-08-19 00:46:19 +0900

    2022 0819 템플릿 업데이트

--- next.config.js
+++ next.config.js
@@ -6,4 +6,11 @@ module.exports = {
   eslint: {
     dirs: [],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [ '@svgr/webpack', ],
+    });
+    return config;
+  },
 };
