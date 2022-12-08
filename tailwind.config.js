ff9ec40d05b1bad912bc2ca508fa9fa152f066b6
NIHILncunia <nihil_ncunia@naver.com>
2022-12-09 00:44:41 +0900

    2022 1209 템플릿 업데이트

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: [ 'Noto Sans KR', 'sans-serif', ],
+        fa: [ 'Font Awesome 5 Free', 'sans-serif', ],
       },
       screens: {
         xs: { max: '479px', },
